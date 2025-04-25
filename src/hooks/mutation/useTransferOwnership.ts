aed21c3ce4093ada0f49c2cfa5b55d8f15025094
FjrREPO <jossfajar21@gmail.com>
2025-04-25 11:11:33 +0700

    feat: implement useTransferOwnership hook for managing ownership transfers

--- src/hooks/mutation/useTransferOwnership.ts
+++ src/hooks/mutation/useTransferOwnership.ts
@@ -1,21 +1,16 @@
 import { ContractRentRegistrar } from "@/constans/contracts";
 import { RentRegistrarABI } from "@/lib/abis/RentRegistrarABI";
-import api from "@/lib/api";
 import { config } from "@/lib/wagmi";
-import { CreatePNS } from "@/types/create-pns.type";
 import { useMutation } from "@tanstack/react-query";
-import { waitForTransactionReceipt, writeContract, readContract } from "@wagmi/core";
+import { waitForTransactionReceipt, writeContract } from "@wagmi/core";
 import { useState } from "react";
 import { parseGwei } from "viem";
-import { useAccount } from "wagmi";
 
 type Status = "idle" | "loading" | "success" | "error";
 type HexAddress = `0x${string}`;
 
 export const useRegisterPNS = () => {
   const [txHash, setTxHash] = useState<HexAddress | null>(null);
-
-  const { address } = useAccount()
 
   const [steps, setSteps] = useState<
     Array<{
@@ -27,18 +22,16 @@ export const useRegisterPNS = () => {
     {
       step: 1,
       status: "idle",
-    },
-    {
-      step: 2,
-      status: "idle",
     }
   ]);
 
   const mutation = useMutation({
     mutationFn: async ({
-      name
+      name,
+      toAddress
     }: {
       name: string;
+      toAddress: HexAddress;
     }) => {
       try {
         setSteps([
@@ -55,63 +48,14 @@ export const useRegisterPNS = () => {
           })
         );
 
-        const response: CreatePNS = await api.post(`create-pns`, { name: `${name}.pharos` });
-
-        setSteps((prev) =>
-          prev.map((item) => {
-            if (item.step === 1) {
-              return { ...item, status: "success" };
-            }
-            return item;
-          })
-        );
-
-        setSteps((prev) =>
-          prev.map((item) => {
-            if (item.step === 2) {
-              return { ...item, status: "loading" };
-            }
-            return item;
-          })
-        );
-
-        // const registrationFee = await readContract(config, {
-        //   address: ContractDomainNameNFT,
-        //   abi: DomainNameNFTABI,
-        //   functionName: "registrationFee",
-        // }) as bigint;
-
-        // const txHash = await writeContract(config, {
-        //   address: ContractDomainNameNFT,
-        //   abi: DomainNameNFTABI,
-        //   functionName: "registerDomain",
-        //   args: [
-        //     `${name}.pharos`,
-        //     response.metadataURI
-        //   ],
-        //   value: registrationFee,
-        // });
-
-        const price = await readContract(config, {
-          address: ContractRentRegistrar,
-          abi: RentRegistrarABI,
-          functionName: "rentPrice",
-          args: [
-            BigInt(1)
-          ]
-        }) as bigint;
-
         const txHash = await writeContract(config, {
           address: ContractRentRegistrar,
           abi: RentRegistrarABI,
-          functionName: "register",
+          functionName: "transferOwnership",
           args: [
             `${name}`,
-            address,
-            BigInt(1),
-            response.metadataURI
+            toAddress
           ],
-          value: price,
           gas: BigInt(300_000), 
           maxFeePerGas: parseGwei('10'), 
           maxPriorityFeePerGas: parseGwei('2'),
@@ -125,7 +69,7 @@ export const useRegisterPNS = () => {
 
         setSteps((prev) =>
           prev.map((item) => {
-            if (item.step === 2) {
+            if (item.step === 1) {
               return { ...item, status: "success" };
             }
             return item;
